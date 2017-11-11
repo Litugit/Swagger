344ba1ecb2fb3c891238dcec019f9d73821cad9a
Litu <litu.nayakk@gmail.com>
2017-11-11 23:28:02 +0530

    spring boot swagger2 Application

---
+++ spring-boot-swagger2/src/main/java/com/example/springbootswagger2/SpringBootSwagger2Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootswagger2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// https://howtodoinjava.com/swagger2/swagger-spring-mvc-rest-example/
+
+@SpringBootApplication
+public class SpringBootSwagger2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSwagger2Application.class, args);
+	}
+}
